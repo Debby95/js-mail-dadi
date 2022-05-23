0e916a33b108a99f6d798d528c25840cb23d8280
Debby95 <debbyjacksonstar@hotmail.it>
2022-05-23 16:43:41 +0200

    chiede all utente la mail

--- mail/app.js
+++ mail/app.js
@@ -2,3 +2,8 @@
 const listaEmail = ["pippo@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "paperino@gmail.com"];
 
 console.log(listaEmail);
+
+//chiedo all utente di inserire la sua email
+const emailUser = prompt("Qual è la tua email?")
+
+
